37f67702685e7cddeb4e48a87127702f2ab45706
Eric Tweedle <erictweeds@gmail.com>
2019-01-14 22:53:05 -0500

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 from app import server
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get("PORT", 33507))
+app.run(debug=True, port=port)
