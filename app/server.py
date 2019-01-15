3e3d84f1ee6b09eccdcbe652cf5aba75443d1c9c
Eric Tweedle <erictweeds@gmail.com>
2019-01-14 22:20:53 -0500

    remove hue functionality

--- app/server.py
+++ app/server.py
@@ -5,7 +5,7 @@ import forecastio
 import json
 import random
 
-b = Bridge('192.168.2.2')
+#b = Bridge('192.168.2.2')
 #b.connect() #first connect only
 lightsDict = {
     "overhead1": 1,
@@ -75,91 +75,91 @@ def lightTypes():
 
     return json.dumps(response)
 
-@app.route('/lights', methods= ['PUT'])
-def lights():
-    #reset all
-    b.set_light(lightsDict["overhead1"], "on", False)
-    b.set_light(lightsDict["overhead2"], "on", False)
-    b.set_light(lightsDict["lamp"], "on", False)
+# @app.route('/lights', methods= ['PUT'])
+# def lights():
+#     #reset all
+#     b.set_light(lightsDict["overhead1"], "on", False)
+#     b.set_light(lightsDict["overhead2"], "on", False)
+#     b.set_light(lightsDict["lamp"], "on", False)
 
-    mode = request.headers.get('type')
-    if (mode == "studyMode"):
-        command = {
-            "on": True,
-            "bri": 254,
-            "sat": 0
-        }
-        b.set_light(lightsDict["overhead1"], command)
-        b.set_light(lightsDict["overhead2"], command)
-        b.set_light(lightsDict["lamp"], command)
+#     mode = request.headers.get('type')
+#     if (mode == "studyMode"):
+#         command = {
+#             "on": True,
+#             "bri": 254,
+#             "sat": 0
+#         }
+#         b.set_light(lightsDict["overhead1"], command)
+#         b.set_light(lightsDict["overhead2"], command)
+#         b.set_light(lightsDict["lamp"], command)
 
-    elif (mode == "siren"):
-        leftCommand = {
-            "on": True, 
-            "hue": 0,
-            "sat": 200,
-            "alert": "lselect"
-        }
-        rightCommand = {
-            "on": True, 
-            "hue": 48000,
-            "sat": 254,
-            "alert": "lselect"
-        }
-        b.set_light(lightsDict["overhead1"], leftCommand)
-        b.set_light(lightsDict["overhead2"], rightCommand)
-        b.set_light(lightsDict["lamp"], "on", False)
+#     elif (mode == "siren"):
+#         leftCommand = {
+#             "on": True, 
+#             "hue": 0,
+#             "sat": 200,
+#             "alert": "lselect"
+#         }
+#         rightCommand = {
+#             "on": True, 
+#             "hue": 48000,
+#             "sat": 254,
+#             "alert": "lselect"
+#         }
+#         b.set_light(lightsDict["overhead1"], leftCommand)
+#         b.set_light(lightsDict["overhead2"], rightCommand)
+#         b.set_light(lightsDict["lamp"], "on", False)
     
-    elif (mode == "disco"):
-        overhead1 = {
-            "on": True, 
-            "hue": 0,
-            "sat": 254,
-            "effect": "colorloop"
-        }
-        overhead2 = {
-            "on": True, 
-            "hue": 21845,
-            "sat": 254,
-            "effect": "colorloop"
-        }
-        lamp = {
-            "on": True, 
-            "hue": 43690,
-            "sat": 254,
-            "effect": "colorloop"
-        }
-        b.set_light(lightsDict["overhead1"], overhead1)
-        b.set_light(lightsDict["overhead2"], overhead2)
-        b.set_light(lightsDict["lamp"], lamp)
+#     elif (mode == "disco"):
+#         overhead1 = {
+#             "on": True, 
+#             "hue": 0,
+#             "sat": 254,
+#             "effect": "colorloop"
+#         }
+#         overhead2 = {
+#             "on": True, 
+#             "hue": 21845,
+#             "sat": 254,
+#             "effect": "colorloop"
+#         }
+#         lamp = {
+#             "on": True, 
+#             "hue": 43690,
+#             "sat": 254,
+#             "effect": "colorloop"
+#         }
+#         b.set_light(lightsDict["overhead1"], overhead1)
+#         b.set_light(lightsDict["overhead2"], overhead2)
+#         b.set_light(lightsDict["lamp"], lamp)
 
-    elif(mode == "reading"):
-        overhead = {
-            "on": True, 
-            "bri": 10,
-            "hue": 0,
-            "sat": 50
-        }
-        lamp = {
-            "on": True, 
-            "bri": 100,
-            "hue": 0,
-            "sat": 50
-        }
-        b.set_light(lightsDict["overhead1"], overhead)
-        b.set_light(lightsDict["overhead2"], overhead)
-        b.set_light(lightsDict["lamp"], lamp)
+#     elif(mode == "reading"):
+#         overhead = {
+#             "on": True, 
+#             "bri": 10,
+#             "hue": 0,
+#             "sat": 50
+#         }
+#         lamp = {
+#             "on": True, 
+#             "bri": 100,
+#             "hue": 0,
+#             "sat": 50
+#         }
+#         b.set_light(lightsDict["overhead1"], overhead)
+#         b.set_light(lightsDict["overhead2"], overhead)
+#         b.set_light(lightsDict["lamp"], lamp)
 
-    elif(mode == "sexyTime"):
-        command = {
-            "on": True,
-            "bri": 75,
-            "hue": 0,
-            "sat": 225
-        }
-        b.set_light(lightsDict["overhead1"], command)
-        b.set_light(lightsDict["overhead2"], command)
-        b.set_light(lightsDict["lamp"], command)
+#     elif(mode == "sexyTime"):
+#         command = {
+#             "on": True,
+#             "bri": 75,
+#             "hue": 0,
+#             "sat": 225
+#         }
+#         b.set_light(lightsDict["overhead1"], command)
+#         b.set_light(lightsDict["overhead2"], command)
+#         b.set_light(lightsDict["lamp"], command)
 
 if __name__ == '__main__':
     app.run()
